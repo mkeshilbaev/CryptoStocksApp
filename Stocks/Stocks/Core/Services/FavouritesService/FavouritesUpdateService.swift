96d11c3ef8ab2eb2e2cc6f73142d3dc3558a31ef
Madi Keshilbayev <m.keshilbaev@MacBook-Pro-Madi-2.local>
2022-06-10 16:08:04 +0600

    charts done

--- Stocks/Stocks/Core/Services/FavouritesService/FavouritesUpdateService.swift
+++ Stocks/Stocks/Core/Services/FavouritesService/FavouritesUpdateService.swift
@@ -13,10 +13,12 @@ import Foundation
 
 extension FavouritesUpdateServiceProtocol {
 	func startFavoritesNotificationObserving(){
-		NotificationCenter.default.addObserver(self,
-											   selector: #selector(setFavourite),
-											   name: NSNotification.Name.favourites,
-											   object: nil)
+		NotificationCenter.default.addObserver(
+			self,
+			selector: #selector(setFavourite),
+			name: NSNotification.Name.favourites,
+			object: nil
+		)
 	}
 }
 
